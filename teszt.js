27c783ba9375331a79305835a8ae23f0439c9112
retek rák <lest12110@gmail.com>
2023-07-11 17:09:10 +0200

    feladat ujraértelmezése

--- teszt.js
+++ teszt.js
@@ -1,8 +1,16 @@
 export function feladat01(szinek) {
     let len = szinek.length;
-    let rv = 0;
-    for (let i = 1; i <= len; i++) {
-        rv += Math.pow(2, i) - 1;
+    let rv = 0; //return value
+    for (let i = 2; i <= len; i++) {
+        rv += c(len, i);
+    }
+    return rv;
+}
+
+function c(n, k) {
+    let rv = 1;
+    for (let i = 1; i <= k; i++) {
+        rv *= (n - (k - i)) / i;
     }
     return rv;
 }
